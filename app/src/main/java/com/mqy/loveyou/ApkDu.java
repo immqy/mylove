76ccae4e26408c3ae4a22d828f61a3a958bbdf75
杜锦阳 <309933706@qq.com>
2015-09-29 14:32:03 +0800

    testing love

---
+++ app/src/main/java/com/mqy/loveyou/ApkDu.java
@@ -0,0 +1,13 @@
+package com.mqy.loveyou;
+
+/**
+ * Created by dujinyang on 2015/9/29.
+ */
+public class ApkDu {
+
+
+    public static int testingDu(){
+
+        return 100;
+    }
+}
---
+++ love/src/main/java/com/mqy/love/ApkLoveDu.java
@@ -0,0 +1,11 @@
+package com.mqy.love;
+
+/**
+ * Created by pengjianbo on 2015/9/29.
+ */
+public class ApkLoveDu{
+
+        public static int testDUDU(){
+            return 200;
+        }
+}
